e4af9b6f89ce80f0c3103bdbc88bbfbdee967d76
Eliezer de Jesus Oliveira <eliezerdejj@gmail.com>
2018-05-13 14:57:04 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,26 +1,35 @@
 <?php
-    header("Content-Type: text/html; charset=ISO-8859-1",true);
-    if(!$fp=fopen("https://www.infomoney.com.br/mercados/cambio" , "r" )) 
-    {
-        echo "Erro ao buscar dados" ;
-        exit;
-    }
-    $conteudo = '';
-    while(!feof($fp)) 
-    { 
-        $conteudo .= fgets($fp,1024);
-    }
-    fclose($fp);
-    $valorCompraHTML = explode('class="numbers">', $conteudo); 
-    $valorCompra = trim(strip_tags($valorCompraHTML[1]));
-    $valorVendaHTML = explode('+', strip_tags($valorCompraHTML[2]));
-    //Valores HTML para exibir no site.	
-    $valorVendaHTML = explode('-', $valorVendaHTML[0]);
-    $valorVenda  = trim($valorVendaHTML[0]) ;
-    //Valores numéricos para cálculos.	  
-    $valorCompraCalculavel = str_replace(',','.', $valorCompra);
-    $valorVendaCalculavel  = str_replace(',','.', $valorVenda);
-?> 
+
+  if(!$fp=fopen("https://www.infomoney.com.br/mercados/cambio" , "r" )) 
+  {
+    echo "Erro ao abrir a página de cotação" ;
+    exit;
+  }
+
+  $conteudo = '';
+  while(!feof($fp)) 
+  { 
+    $conteudo .= fgets($fp,1024);
+  }
+  fclose($fp);
+
+  $valorCompraHTML = explode('<td><span>', $conteudo); 
+  $valorCompra = trim(strip_tags($valorCompraHTML[1]));
+  $valorVendaHTML = explode('+', strip_tags($valorCompraHTML[2]));
+
+// Dolar comercial posicao 1 e 2
+// Euro posicao 7 e 8
+// Peso Argentino Posicao 13 e 14
+
+  //Estes são os valores HTML para exibir no site.  
+  $valorVendaHTML = explode('-', $valorVendaHTML[0]);
+  $valorVenda  = trim($valorVendaHTML[0]) ;
+
+  //Estes são os valores numéricos para cálculos.     
+  $valorCompraCalculavel = str_replace(',','.', $valorCompra);
+  $valorVendaCalculavel  = str_replace(',','.', $valorVenda);
+
+?>
 
 <!DOCTYPE html>
 <html lang="pt-br">
